e081392ba4132839dd84bae62faf3d96f64abae7
Grace Kasahara <gracekasahara@gmail.com>
2018-06-22 16:49:41 -0500

    Added functions for adding a row to the table with a button click, also added delete buttons

--- client.js
+++ client.js
@@ -1,79 +1,57 @@
-class Employee{
-    constructor( firstName, lastName, idNumber, jobTitle, annualSalary ){
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.idNumber = idNumber;
-      this.jobTitle = jobTitle;
-      this.annualSalary = annualSalary;
-    } // end constructor
-  } // end Employee class
-  const employees = [];
-
-
-
-$(document).ready( readyNow );
-
-function readyNow() {
-    addClickHandlers();
-    console.log('jquery ready to rock and or roll.');
-    //Append cohorts to DOM
-        //loop through array
-        //for(let i = 0; i < cohorts.length; i++)
-        for (let cohort of cohorts){
-            //append cohort info to table
-            //$('tr')//selecting all tr on the dom
-            //$('<tr></tr>')making a new element not on DOM
-            //let $row = $('<tr></tr>'); //creates a table row
-            //cohort name, status, number
-            //$row.append('<td>' + cohort.cohortName + '</td>');
-            //$row.append('<td>' + cohort.status + '</td>');
-            //$row.append('<td>' + cohort.numberOfStudents + '</td>');
-            //$row.append(`<td><button class="deleteBtn">Delete</button></td>`);
-            //template literal, ES6
-            //$row.append(`<td>${cohort.numberOfStudents}</td>`)
-
-            //add to table
-            //$('#cohortTable').append($row);
-
-            addRow(cohort.cohortName, cohort.status, cohort.numberOfStudents);
-        }
+class Employee {
+    constructor(firstName, lastName, idNumber, jobTitle, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.jobTitle = jobTitle;
+        this.annualSalary = annualSalary;
+    }
 }
 
-function addClickHandlers(){
-    //On click or delete, delete row
-    //need to target a parent already on the table
-    $('#cohortTable').on('click', '.deleteBtn', handleDelete);
-    //On click of submit add row
+const employees = [];
+
+let totalMonthly = 0;
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    $('#totalMonthly').append(totalMonthly);
+    addClickHandlers();
+    for (let employee of employees) {
+        addRow(employee.firstName, employee.lastName, employee.idNumber, employee.jobTitle, employee.annualSalary);
+    }
+}
+
+function addClickHandlers() {
+    $('#employeeTable').on('click', '.deleteBtn', handleDelete);
     $('#submit').on('click', handleSubmit);
 }
 
-function handleSubmit(){
-    //collect input values, add row, clear inputs
-    let name = $('#name').val();
-    let status = $('#status').val();
-    let numStudents = $('#numStudents').val();
-    addRow(name, status, numStudents);
-    $('#name').val('');
-    $('#status').val('');
-    $('#numStudents').val('');
+function handleSubmit() {
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let idNumber = $('#idNumber').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
+    addRow(firstName, lastName, idNumber, jobTitle, annualSalary);
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNumber').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
 }
 
-function handleDelete(){
-    console.log('clicked');
-    //Remove the row .remove()
-    //$(this).parent().parent().remove();
-    //find and remove a specific parent
-    //$(this).parents('tr').remove();
-    //remove the closest thing to me with tr
+function handleDelete() {
     $(this).closest('tr').remove();
 }
 
-function addRow(name, status, numStudents){
-    //append to DOM
+function addRow(firstName, lastName, idNumber, jobTitle, annualSalary) {
     let $row = $('<tr></tr>');
-    $row.append('<td>' + name + '</td>');
-    $row.append('<td>' + status + '</td>');
-    $row.append('<td>' + numStudents + '</td>');
+    $row.append('<td>' + firstName + '</td>');
+    $row.append('<td>' + lastName + '</td>');
+    $row.append('<td>' + idNumber + '</td>');
+    $row.append('<td>' + jobTitle + '</td>');
+    $row.append('<td>' + annualSalary + '</td>');
     $row.append(`<td><button class="deleteBtn">Delete</button></td>`);
-    $('#cohortTable').append($row);
+    $('#employeeTable').append($row);
 }
